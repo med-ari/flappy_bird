f2185af1c9241ff4358117c23cc2b884c9e64a0e
med-ari <131400549+med-ari@users.noreply.github.com>
2024-09-16 19:49:01 -0300

    bird update

---
+++ README.md
@@ -0,0 +1,2 @@
+# flappy_bird
+flappy bird game in love2d following cs50
--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 push = require "push"
+Class = require "class"
+require "Bird"
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -16,19 +18,25 @@ local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
 
+love.keyboard.keysPressed = {}
+
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   love.window.setTitle("Flappity Birdo")
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
-    })
+  })
+
+  pajarito = Bird()
 end
 
 function love.update(dt)
   backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
   groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+  pajarito:update(dt)
+  love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -36,14 +44,21 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+  love.keyboard.keysPressed[key] = true
+
   if key == 'escape'then
     love.event.quit()
   end
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
 end
 
 function love.draw()
   push:start()
   love.graphics.draw(background, -backgroundScroll, 0)
   love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+  pajarito:render()
   push:finish()
 end
